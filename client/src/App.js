71efd77726a215255528e44a92ed1bf37876a516
Dupe Fadina <maafadina@hotmail.com>
2019-08-31 14:57:29 -0700

    Adding files to remote repo(React-Reading-List)

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Books} />
+          <Route path="/books" exact component={Books} />
+          <Route path="/books/:id" exact component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
